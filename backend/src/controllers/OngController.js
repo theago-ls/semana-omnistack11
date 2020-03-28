71b551d8fdc4fd0adc48a0243b5bdc54a8e9bc2d
Thiago Lourenço <thiagosup1@gmail.com>
2020-03-28 04:41:45 -0400

    Creating front-end

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -8,7 +8,7 @@ module.exports = {
         return res.json(ongs);
     },
 
-    async create(){
+    async create(req, res){
         const { name, email, whatsapp, city, uf } = req.body;
 
         const id = crypto.randomBytes(4).toString('HEX');
